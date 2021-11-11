16e83454600d4eeb57d32eb9a05ae1af8f1ef716
Aliona990220 <aliona.havela@gmail.com>
2021-11-11 16:54:42 +0200

    data types

---
+++ src/main/java/TypesOfData.java
@@ -0,0 +1,12 @@
+public class TypesOfData {
+    public static void main(String[] args) {
+        byte a = 5; // -128 ,127
+        short b = -100;
+        int c ;
+        c= 1000000;
+        long d = -123456789;
+        
+
+
+    }
+}
