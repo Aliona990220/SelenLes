58a5da51c846c22888c2f6304dafbd57243e4975
Aliona990220 <aliona.havela@gmail.com>
2021-11-11 17:47:29 +0200

    simple operations

--- src/main/java/SimpleOperations.java
+++ src/main/java/SimpleOperations.java
@@ -1,5 +1,16 @@
 public class SimpleOperations {
     public static void main(String[] args) {
-        
+        int a;
+        int b;
+        a = 15;
+        b = 10;
+        System.out.println(a == b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(a != b);
+        System.out.println(a + 5 == b + 10);
+        System.out.println(!(a + 5 == b + 10));
+        System.out.println(a < b);
+        System.out.println(a > b);
     }
 }
