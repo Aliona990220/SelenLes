573313411a782000c9f9a24e3ddbfbc67e62233f
Aliona990220 <aliona.havela@gmail.com>
2021-11-11 19:11:15 +0200

    conditions

---
+++ src/main/java/Conditions.java
@@ -0,0 +1,40 @@
+public class Conditions {
+    public static void main(String[] args) {
+//        int i = 10;
+//        System.out.println("Start");
+//        if (i > 10){
+//            System.out.println("i > 10");
+//            i++;
+//            System.out.println(i);
+//        }
+//        else if (i < 10 && i > 5){
+//            System.out.println("i < 10 & i > 5");
+//            i--;
+//            System.out.println(i);
+//
+//        }
+//        else {
+//            System.out.println("Nothing");
+//        }
+//        System.out.println("End");
+
+
+
+        int i = 2;
+
+        switch (i){
+            case 1:
+                System.out.println("The number is 1");
+                break;
+            case 2:
+                System.out.println("The number is 2");
+                break;
+            case 3:
+                System.out.println("The number is 3");
+                break;
+            default:
+                System.out.println("I don`t know the nomber");
+
+        }
+    }
+}
