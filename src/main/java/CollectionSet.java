de1eb201e405a1d3b414fc90571a3e34ee73a4a9
Aliona990220 <aliona.havela@gmail.com>
2021-11-12 18:23:50 +0200

    collection set

---
+++ src/main/java/CollectionSet.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionSet {
+    public static void main(String[] args) {
+
+        Set<Integer> set = new HashSet<>();
+        set.add(2);
+        set.add(4);
+        set.add(10);
+        set.add(11);
+
+        System.out.println(set.size());
+        Iterator iterator = set.iterator();
+        while (iterator.has)
+
+
+    }
+}
--- src/main/java/Iterator.java
+++ src/main/java/Iterator.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Iterator {
+    public boolean has;
+
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
