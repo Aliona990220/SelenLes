625be18bc188138d6ceba731a3b6549f9957147e
Aliona990220 <aliona.havela@gmail.com>
2021-11-16 23:42:07 +0200

    Encapsulation

---
+++ src/main/java/SomePhone.java
@@ -0,0 +1,22 @@
+public class SomePhone {
+    private int year;
+    private String company;
+    public SomePhone(int year, String company) {
+        this.year = year;
+        this.company = company;
+    }
+    private void openConnection(){
+        //findComutator
+        //openNewConnection...
+    }
+    public void call() {
+        openConnection();
+        System.out.println("Вызываю номер");
+    }
+
+    public void ring() {
+        System.out.println("Дзынь-дзынь");
+    }
+
+}
+
